edf2769edfe6b812d987f203dcf90c3d90173abb
Mona Lisa <Mona Lisa>
2022-09-28 17:22:21 +0300

    delete all

--- iptables.fix.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-sudo iptables -t filter -F
-sudo iptables -t filter -X
-systemctl restart docker
--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-echo "Go mod inited"
-screen -dmS golang-user go run cmd/app/*.go
-echo "Finished"
